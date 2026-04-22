e5d4e2a7a7f362e17d47960d60810864f5d60a66
DATA GURU <gyrogodnon@gmail.com>
2026-04-22 15:42:19 +0530

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -8,3 +8,13 @@ def calculate(a,b):
       password = "admin123"
       result = eval(a+b)
       return result
+def calculate(a,b):
+      password = "admin123"
+      result = eval(a+b)
+      return result
+
+
+def calculate(a,b):
+      password = "admin123"
+      result = eval(a+b)
+      return result
